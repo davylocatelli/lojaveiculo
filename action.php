4fa7b9eedafe8ff757cf620a5583c66c681f690e
D <142498566+davylocatelli@users.noreply.github.com>
2024-09-21 00:33:53 +0000

    conexaocompleta

--- action.php
+++ action.php
@@ -27,4 +27,40 @@ else
 }
 mysqli_close($conn);
 }
+if(isset($_POST['save2'])) 
+{
+  $cdcliente = $_POST['cdcliente'];     
+  $nmcliente = $_POST['nmcliente'];
+  $emailcliente = $_POST['emailcliente'];     
+  $endcliente = $_POST['endcliente'];
+
+$sql_query = "INSERT INTO cliente (cdcliente,nmcliente,emailcliente,endcliente) VALUES ('$cdcliente','$nmcliente',' $emailcliente','$endcliente')";
+if(mysqli_query($conn,$sql_query))
+{
+    echo "Cliente cadastrado com sucesso!";
+}
+else
+{
+    echo "error" . $sql . "". mysqli_error($conn);
+}
+mysqli_close($conn);
+}
+if(isset($_POST['save3'])) 
+{
+  $cdfabricante = $_POST['cdfabricante'];     
+  $nmfabricante = $_POST['nmfabricante'];
+  $emailfabricante = $_POST['emailfabricante'];     
+  $endfabricante = $_POST['endfabricante'];
+
+$sql_query = "INSERT INTO fabricante (cdfabricante,nmfabricante,emailfabricante,endfabricante) VALUES ('$cdfabricante','$nmfabricante',' $emailfabricante','$endfabricante')";
+if(mysqli_query($conn,$sql_query))
+{
+    echo "Fabricante cadastrado com sucesso!";
+}
+else
+{
+    echo "error" . $sql . "". mysqli_error($conn);
+}
+mysqli_close($conn);
+}
 ?>
